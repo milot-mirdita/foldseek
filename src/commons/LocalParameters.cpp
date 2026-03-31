e870222b34c92da2d123252b81ae51964b94338c
Michel van Kempen <michel.van-kempen@mpinat.mpg.de>
2026-03-31 10:50:35 +0200

    Add --ss-12st flag to structurealign.
    12st seqs can be disabled even so the database contains 12st
    sequences. Important: 12st seqs are disabled by default.

--- src/commons/LocalParameters.cpp
+++ src/commons/LocalParameters.cpp
@@ -157,6 +157,8 @@ LocalParameters::LocalParameters() :
     structurerescorediagonal.push_back(&PARAM_TMSCORE_THRESHOLD_MODE);
     structurerescorediagonal.push_back(&PARAM_LDDT_THRESHOLD);
     structurerescorediagonal.push_back(&PARAM_ALIGNMENT_TYPE);
+    structurerescorediagonal.push_back(&PARAM_SS_12ST);
+    structurerescorediagonal.push_back(&PARAM_SUBMAT_12ST_SCALE);
     structurerescorediagonal = combineList(structurerescorediagonal, align);
 
     structurealign.push_back(&PARAM_TMSCORE_THRESHOLD);
@@ -166,6 +168,7 @@ LocalParameters::LocalParameters() :
     structurealign.push_back(&PARAM_ALIGNMENT_TYPE);
     structurealign.push_back(&PARAM_EXACT_TMSCORE);
     structurealign.push_back(&PARAM_SUBMAT_12ST_SCALE);
+    structurealign.push_back(&PARAM_SS_12ST);
     structurealign = combineList(structurealign, align);
 
     // strucclust
@@ -413,7 +416,7 @@ LocalParameters::LocalParameters() :
     // 12st substitution matrix scale
     submat12stScale = 2.1;
 
-    // include 12-state alphabet in _ss database (default: enabled)
+    // include 12-state alphabet in _ss database and use for scoring
     ss12st = 1;
 
     citations.emplace(CITATION_FOLDSEEK, "van Kempen, M., Kim, S.S., Tumescheit, C., Mirdita, M., Lee, J., Gilchrist, C.L.M., Söding, J., and Steinegger, M. Fast and accurate protein structure search with Foldseek. Nature Biotechnology, doi:10.1038/s41587-023-01773-0 (2023)");
